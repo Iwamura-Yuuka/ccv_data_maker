3399588afe3ab807fcb9924d871a44cefced6cb2
Iwamura-Yuuka <ee203071@meiji.ac.jp>
2024-01-10 17:21:33 +0900

    add comment

--- exe_time_publisher/include/exe_time_publisher/exe_time_publisher.h
+++ exe_time_publisher/include/exe_time_publisher/exe_time_publisher.h
@@ -15,11 +15,11 @@ private:
     void robot_odom_callback(const nav_msgs::Odometry::ConstPtr& msg);
     
     // 引数なし関数
-    void calc_exe_time();
+    void calc_exe_time();  // 実行時間を計算
     void show_exe_time();  // 実行時間を表示
 
     // yamlファイルで設定可能な変数
-    int hz_;                  // ループ周波数 [Hz]
+    int hz_;          // ループ周波数 [Hz]
     double start_x_;  // 計測を開始するx座標 [m]
     double goal_x_;   // 計測を終了するx座標 [m]
 
@@ -32,7 +32,7 @@ private:
 
     // 実行時間関連
     bool flag_clock_start_ = false;  // 計測開始の確認用
-    bool flag_clock_goal_ = false;    // 計測終了の確認用
+    bool flag_clock_goal_ = false;   // 計測終了の確認用
 
     // NodeHandle
     ros::NodeHandle nh_;
